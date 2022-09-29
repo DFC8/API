f87ea9d21793dd92220619e4758b4df609868b3b
DFC8 <darcksonalc04@gmail.com>
2022-09-29 11:18:17 -0300

    interface user repository

---
+++ src/main/java/br/com/dfc8/api/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.dfc8.api.repositories;
+
+import br.com.dfc8.api.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
