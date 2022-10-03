90e91f063c6ab5538de8e7cedae64c8bf668e60c
DFC8 <darcksonalc04@gmail.com>
2022-10-03 16:15:13 -0300

    teste para classe player service metodo findById

---
+++ src/test/java/br/com/dfc8/api/services/impl/PlayerServiceImplTest.java
@@ -0,0 +1,80 @@
+package br.com.dfc8.api.services.impl;
+
+import br.com.dfc8.api.domain.Player;
+import br.com.dfc8.api.domain.dto.PlayerDTO;
+import br.com.dfc8.api.repositories.PlayerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class PlayerServiceImplTest {
+
+    public static final Integer ID = 1;
+       public static final String NAME = "Valdir";
+    public static final String EMAIL = "valdir@gmail.com";
+    public static final String PASSWORD = "123";
+    @InjectMocks
+    private PlayerServiceImpl service;
+
+    @Mock
+    private PlayerRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+    private Player player;
+    private PlayerDTO playerDTO;
+    private Optional<Player> optionalPlayer;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startPlayer();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnPlayerInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalPlayer);
+
+        Player response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(Player.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startPlayer() {
+        player = new Player(ID, NAME, EMAIL, PASSWORD);
+        playerDTO = new PlayerDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalPlayer = Optional.of(new Player(ID, NAME, EMAIL, PASSWORD));
+    }
+}
