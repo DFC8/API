ca254cb52360a9a6a5a6604a0bd7d0342504e753
DFC8 <darcksonalc04@gmail.com>
2022-10-06 10:59:04 -0300

    corrigin bug

--- src/main/java/br/com/dfc8/api/domain/User.java
+++ src/main/java/br/com/dfc8/api/domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(estrategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
