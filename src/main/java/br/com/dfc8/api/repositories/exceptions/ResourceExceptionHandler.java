e817a88fb09787d09866770fa07e8f80cb1ada6b
DFC8 <darcksonalc04@gmail.com>
2022-10-06 10:09:47 -0300

    teste para manipulador de excecoes metodo data integrity

--- src/main/java/br/com/dfc8/api/repositories/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/dfc8/api/repositories/exceptions/ResourceExceptionHandler.java
@@ -20,7 +20,7 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
     @ExceptionHandler(DataIntegratyVaiolationException.class)
-    public ResponseEntity<StandardError>dataIntegratyViolationException(DataIntegratyVaiolationException ex, HttpServletRequest request){
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegratyVaiolationException ex, HttpServletRequest request){
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
                 request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
--- src/test/java/br/com/dfc8/api/repositories/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/dfc8/api/repositories/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.dfc8.api.repositories.exceptions;
 
+import br.com.dfc8.api.services.exceptions.DataIntegratyVaiolationException;
 import br.com.dfc8.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
-    @InjectMocks
+    public static final String E_MAIL_JA_CADASTRADO = "E-mail já cadastrado";
+        @InjectMocks
     private ResourceExceptionHandler resourceExceptionHandler;
 
     @BeforeEach
@@ -41,6 +45,19 @@ class ResourceExceptionHandlerTest {
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = resourceExceptionHandler.dataIntegrityViolationException(
+                new DataIntegratyVaiolationException(E_MAIL_JA_CADASTRADO),
+                new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertNotEquals("/player/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 }
