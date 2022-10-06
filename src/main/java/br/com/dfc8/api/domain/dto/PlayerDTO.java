ff3b8dd6a36474eef83f8cf50f0d951d7424e9c0
DFC8 <darcksonalc04@gmail.com>
2022-10-06 10:10:53 -0300

    removendo anotacoes nao utilizadas

--- src/main/java/br/com/dfc8/api/domain/dto/PlayerDTO.java
+++ src/main/java/br/com/dfc8/api/domain/dto/PlayerDTO.java
@@ -3,12 +3,10 @@ package br.com.dfc8.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class PlayerDTO {
 
     private Integer id;
